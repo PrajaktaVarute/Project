bf8f7d407d5353c77f33d0c99b5718fef45f96dc
Prajakta <varuteprajakta1@gmail.com>
2025-02-11 19:33:18 +0530

    Access Refresh Token , middleware and cookie in backend

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -2,14 +2,15 @@ class ApiError extends Error{
     constructor(
         statusCode,
         message = "Something went wrong",
-        error = [],
+        errors = [],
         stack = ""
     ){
         super(message)
         this.statusCode = statusCode
         this.data  = null
-        this.message = false
-        this.error = error
+        this.message = message
+        this.success = false;
+        this.errors = errors
 
         if(stack){
             this.stack = stack
